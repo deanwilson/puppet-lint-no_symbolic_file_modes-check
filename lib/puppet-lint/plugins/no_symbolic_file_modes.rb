2076d8ee66e9c25d81a51401b1a0b5272bf1704e
Dean Wilson <dean.wilson@gmail.com>
2020-07-10 08:38:12 +0100

    Appease the linter

--- lib/puppet-lint/plugins/no_symbolic_file_modes.rb
+++ lib/puppet-lint/plugins/no_symbolic_file_modes.rb
@@ -1,28 +1,26 @@
 PuppetLint.new_check(:no_symbolic_file_modes) do
-
-  NO_SYMBOLIC_FILE_MODES_IGNORE_TYPES = Set[:VARIABLE, :UNDEF]
-  WARNING = 'mode should be a 4 digit octal value, not a symbolic mode'
+  NO_SYMBOLIC_FILE_MODES_IGNORE_TYPES = Set[:VARIABLE, :UNDEF].freeze
+  WARNING = 'mode should be a 4 digit octal value, not a symbolic mode'.freeze
 
   def check
     resource_indexes.each do |resource|
-      if resource[:type].value == "file"
-        resource[:param_tokens].select { |param_token|
-          param_token.value == 'mode'
-        }.each do |param_token|
-          value_token = param_token.next_code_token.next_code_token
+      next unless resource[:type].value == 'file'
 
-          break if NO_SYMBOLIC_FILE_MODES_IGNORE_TYPES.include?(value_token.type)
-          break if value_token.value =~ /^[0-7]{4}$/
+      param_tokens = resource[:param_tokens].select { |pt| pt.value == 'mode' }
 
-          notify :warning, {
-            :message => WARNING,
-            :line    => value_token.line,
-            :column  => value_token.column,
-            :token   => value_token,
-          }
-        end
+      param_tokens.each do |param_token|
+        value_token = param_token.next_code_token.next_code_token
+
+        break if NO_SYMBOLIC_FILE_MODES_IGNORE_TYPES.include?(value_token.type)
+        break if value_token.value =~ /^[0-7]{4}$/
+
+        notify :warning, {
+          message: WARNING,
+          line: value_token.line,
+          column: value_token.column,
+          token: value_token,
+        }
       end
     end
   end
-
 end
